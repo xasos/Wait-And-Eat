66c403ad22d5635cbbc5e3f182d65baa536520a4
Niraj Pant <nirajpant7@gmail.com>
2014-09-28 20:24:45 -0500

    add text message route

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,14 +7,21 @@ angular.module('myApp.controllers', [])
 
 	}])
 	.controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
-		var partiesRef = new Firebase('https://waitand-eat.firebaseio.com/');
+		var partiesRef = new Firebase('https://waitand-eat.firebaseio.com/parties');
 
 		$scope.parties = $firebase(partiesRef);
-		$scope.party = {name: '', phone: '', size: ''};
+		$scope.newParty = {name: '', phone: '', size: ''};
 		
 		$scope.saveParty = function() {
-			$scope.parties.$add($scope.party);
-			$scope.party = {};
+			$scope.parties.$add($scope.newParty);
+			$scope.newParty = {name: '', phone: '', size: ''};
 		};
+
+		$scope.sendTextMessage = function(phoneNumber) {
+			var textMessageRef = new Firebase('https://waitand-eat.firebaseio.com/textMessages');
+			var textMessages = $firebase(textMessageRef);
+			textMessages.$add({phoneNumber: phoneNumber});
+		};
+		
 	}]);
   	
